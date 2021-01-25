28c2e068c9535d01a550417f3caf1e39fd684591
KontradZebrowsk <konzebr@gmail.com>
2021-01-25 18:05:18 +0000

    Zad5.7

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -6,7 +6,15 @@
 
 // tutaj algorytm liczKolejneWarzywa
 template <typename T>
+bool predic(std::string s, T* it)
+{
+  return s == T->getNazwa();
+}
+
+template <typename T>
 bool liczKolejneWarzywa(T begin, T end, unsigned n, const std::string& s)
 {
-return std::search_n(begin, end, n, s, [](T it){return it.getNazwa() == s;}) != end;
+return std::search_n(begin, end, n, s, predic) != end;
 }
+
+//[](T it){return it.getNazwa() == s;}
