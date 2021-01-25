88fa8a63682a357a750fa0fae2a76ad26440b635
KontradZebrowsk <konzebr@gmail.com>
2021-01-25 16:46:06 +0000

    Zad1.1

---
+++ build/CMakeFiles/kolokwium.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/runner/kolokwium-2-KontradZebrowski1-1/src/tests_main.cpp" "/home/runner/kolokwium-2-KontradZebrowski1-1/build/CMakeFiles/kolokwium.dir/src/tests_main.cpp.o"
+  "/home/runner/kolokwium-2-KontradZebrowski1-1/src/zad1.cpp" "/home/runner/kolokwium-2-KontradZebrowski1-1/build/CMakeFiles/kolokwium.dir/src/zad1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,26 @@
 #include <string>
 
 // tutaj klasa Warzywo
+class Warzywo
+{
+public:
+
+Warzywo(const std::string& n, double c, int k): nazwa(n), cena(c), kolor(k) { ++liczbaWarzyw; }
+
+~Warzywo() { --liczbaWarzyw; }
+
+unsigned getVeg() { return liczbaWarzyw; }
+
+void opis(std::ostream& a) const 
+{
+  a << nazwa <<": "<<cena<<", "<<kolor<<"\n";
+}
+
+private:
+std::string nazwa;
+double cena;
+int kolor;
+inline static unsigned liczbaWarzyw;
+};
+
+//unsigned Warzywo::liczbaWarzyw = 0;
