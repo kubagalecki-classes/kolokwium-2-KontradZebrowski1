e4a43a24bb9702cb65e6ce61cd424fabc10b032a
KontradZebrowsk <konzebr@gmail.com>
2021-01-25 17:11:44 +0000

    Zad2.1

---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_7aa9a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_7aa9a.dir/feature_tests.cxx.o"
+  "cmTC_7aa9a.pdb"
+  "cmTC_7aa9a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_7aa9a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -4,3 +4,38 @@
 #include "chrup.hpp"
 
 // tutaj klasy Ogorek, Zielony i Kiszony
+class Ogorek
+{
+public:
+virtual std::string chrup() = 0;
+};
+
+class Kiszony : public Ogorek
+{
+public:
+std::string chrup()
+{
+return chrupKiszony();
+}
+};
+
+class Zielony : public Ogorek
+{
+public:
+std::string chrup()
+{
+  return chrupZielony();
+}
+};
+
+std::string jedzOgorek( Ogorek* ogor )
+{
+  if (dynamic_cast<Zielony*>(ogor) != nullptr)
+  {
+    return"Zielony: " + Ogorek->chrup ;
+  }
+  else if (dynamic_cast<Kiszony*>(ogor) != nullptr)
+  {
+    return "Kiszony: " + Ogorek->chrup ;
+  }
+}
